6b6489db87c8d024c53ba5f4debb155985a41512
KAldhalimi <karrarmajed@gmail.com>
2019-01-29 18:56:09 -0500

    initial

---
+++ Movie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11GC
+{
+    class Movie
+    {
+        #region DataMembers
+        private string title;
+        private string category;
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            set
+            {
+                title = value;
+            }
+            get
+            {
+                return title;
+            }
+        }
+        public string Category
+        {
+            set
+            {
+                category = value;
+            }
+            get
+            {
+                return category;
+            }
+        }
+        #endregion
+
+       
+
+        public Movie(string t, string c)
+        {
+            Title = t;
+            Category = c;
+        }
+       
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Movie: " + Title);
+        }
+
+
+
+    }
+}
